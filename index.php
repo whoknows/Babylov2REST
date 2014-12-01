a5e7236afb88cabd986822c0894cf087eb997d4a
gcoste <guillaume.coste@livingobjects.fr>
2014-12-01 17:57:36 +0100

    recuperation des creneaux

--- index.php
+++ index.php
@@ -6,14 +6,36 @@ $app = new \Slim\Slim(array(
     'debug' => true
 ));
 
-$app->get('/users', function () use ($app) {
+$app->get('/users', function () {
     echo json_encode(array_values(\Models\User::getFullList("WHERE enabled = 1")));
 });
 
-$app->get('/users/:id', function ($id) use ($app) {
-    echo json_encode(array_values(\Models\User::getFullList("WHERE a.id = $id")));
+$app->get('/users/:id', function ($id) {
+    $user = \Models\User::getFullList("WHERE a.id = $id");
+
+    if (sizeof($user) == 1) {
+        $user = $user[$id];
+    }
+
+    echo json_encode($user);
 });
 
-//$app->get('/users/:id', 'totoMaFonction');
+/**
+ * Planification
+ */
+$app->get('/slot', function () {
+    echo json_encode(array_values(\Models\Slot::getAllSlots()));
+});
+
+$app->put('/slot/:creneau/:user_id', function ($creneau, $user_id) {
+    echo json_encode(array($creneau, $user_id));
+    //echo json_encode(array_values(\Models\Slot::getAllSlots()));
+});
+
+$app->delete('/slot/:creneau/:user_id', function ($creneau, $user_id) {
+    echo json_encode(array($creneau, $user_id));
+    //echo json_encode(array_values(\Models\Slot::getAllSlots()));
+});
+
 $app->response()->header('Content-Type', 'application/json');
 $app->run();
--- src/Models/Slot.php
+++ src/Models/Slot.php
@@ -1,9 +1,39 @@
 <?php
 
-namespace Model;
+namespace Models;
 
 class Slot
 {
 
-    //
+    public static function getAllSlots($filter = "")
+    {
+        $sql = "SELECT name, user_id, b.id
+                FROM slot a
+                LEFT JOIN `users_slots` b ON a.id = b.`slot_id` AND date = CURDATE()
+                $filter
+                ORDER BY a.name";
+
+        $bdd = \Config\Database::getInstance();
+        $req = $bdd->getConnection()->query($sql);
+
+        return self::formatData($req->fetchAll(\PDO::FETCH_ASSOC));
+    }
+
+    public static function formatData($data)
+    {
+        $ret = array();
+
+        foreach ($data as $slot) {
+            if (!isset($ret[$slot['name']])) {
+                $ret[$slot['name']]['creneau'] = $slot['name'];
+                $ret[$slot['name']]['users'] = array();
+            }
+
+            if($slot['user_id'] != null) {
+                $ret[$slot['name']]['users'][] = $slot['user_id'];
+            }
+        }
+
+        return $ret;
+    }
 }
